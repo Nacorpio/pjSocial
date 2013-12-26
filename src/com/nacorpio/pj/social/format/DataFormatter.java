b687e62985b69943f54df455efb8aaae772b39ba
LightifyLife <mike@lightifylife.com>
2013-12-26 16:20:43 +0100

    Sync

--- src/com/nacorpio/pj/social/format/DataFormatter.java
+++ src/com/nacorpio/pj/social/format/DataFormatter.java
@@ -10,11 +10,11 @@ public class DataFormatter {
 	public static final boolean isEmpty(String... par1) {
 		return (par1.length == 0 || par1 == null);
 	}
-	
+
 	public static final boolean isEmpty(String par1) {
 		return (par1 == null || par1.length() == 0 || par1 == "" || par1.isEmpty());
 	}
-	
+
 	public static final String toFormat(String par1, String... par2) {
 		String var1 = "";
 		if (!isEmpty(par1) && !isEmpty(par2)) {
@@ -22,13 +22,13 @@ public class DataFormatter {
 		}
 		return var1;
 	}
-	
+
 	public static final boolean instanceOf(String par1, String par2) {
 		return par1.contains("[" + par2 + ":{") && par1.endsWith("}]");
 	}
-	
+
 	// //
-	
+
 	public static final String toString(Object... par1) {
 		String var1 = "";
 		for (int i = 0; i < par1.length; i++) {
@@ -48,7 +48,7 @@ public class DataFormatter {
 		}
 		return var1;
 	}
-	
+
 	public static final String toString(ItemStack... par1) {
 		String var1 = "";
 		for (int i = 0; i < par1.length; i++) {
@@ -68,21 +68,21 @@ public class DataFormatter {
 		}
 		return var1;
 	}
-	
+
 	public static final String toSimpleString(Location par1) {
 		return "(" + toString(par1.getBlockX(), par1.getBlockY(), par1.getBlockZ(), par1.getYaw(), par1.getPitch()) + ")";
 	}
-	
+
 	public static final String toSimpleString(ItemStack par1) {
 		return "(" + toString(par1.getType(), par1.getAmount(), par1.getItemMeta().getDisplayName(), par1.getItemMeta().getLore()) + ")";
 	}
-	
+
 	public static final String toSimpleString(Inventory par1) {
 		return "(" + toString(par1.getContents()) + ")";
 	}
-	
+
 	public static final String toString(Player par1) {
 		return toFormat("Player", "Name:" + par1.getName(), "Location:" + toSimpleString(par1.getLocation()), "Inventory:" + toSimpleString(par1.getInventory()));
 	}
-	
+
 }
