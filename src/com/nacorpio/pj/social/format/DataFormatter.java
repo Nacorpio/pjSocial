b11e95b98f04512e16bb47e32e1d5c782c657239
Nacorpio <nacorpio@live.com>
2013-12-26 16:49:55 +0100

    Modified the formatter class and fixed some bugs

--- src/com/nacorpio/pj/social/format/DataFormatter.java
+++ src/com/nacorpio/pj/social/format/DataFormatter.java
@@ -1,18 +1,76 @@
 package com.nacorpio.pj.social.format;
 
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
 import org.bukkit.Location;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 public class DataFormatter {
 
+	public static final String VAR_SEP = "->";
+	public static final String SEPARATOR = "&";
+	
+	static class FormatValue {
+		
+		private String name;
+		private String[] values = new String[]{};
+		
+		public FormatValue(String par1, String... par2) {
+			if (!isEmpty(par1) && !isEmpty(par2)) {
+				this.name = par1;
+				this.values = par2;
+			}
+		}
+		
+		public final void addValue(String par1) {
+			Arrays.asList(values).add(par1);
+		}
+		
+		public final String getName() {
+			return name;
+		}
+		
+		public final String[] getValues() {
+			return values;
+		}
+		
+	}
+	
 	public static final boolean isEmpty(String... par1) {
 		return (par1.length == 0 || par1 == null);
 	}
 	
 	public static final boolean isEmpty(String par1) {
 		return (par1 == null || par1.length() == 0 || par1 == "" || par1.isEmpty());
+	}
+	
+	// [Player:{Name->Aazk&Health->100&Location->(0&0&0&0&0)}]
+	
+	public static final FormatValue toFormatValue(String par1) {
+		
+		String name = "";
+		String values[] = new String[]{};
+		
+		if (isValidFormat(par1)) {
+			
+			String var1 = par1.split(Pattern.quote("["))[1].split(Pattern.quote("]"))[0];
+			String var2 = var1.split(Pattern.quote("{"))[1].split(Pattern.quote("}"))[0];
+			
+			name = var1.split(Pattern.quote(":"))[0];
+			values = var2.split(Pattern.quote(SEPARATOR));
+			
+		}
+		
+		return new FormatValue(name, values);
+		
+	}
+	
+	public static final boolean isValidFormat(String par1) {
+		return (par1.startsWith("[") && par1.endsWith("}]") && par1.contains(":{"));
 	}
 	
 	public static final String toFormat(String par1, String... par2) {
@@ -34,13 +92,33 @@ public class DataFormatter {
 		for (int i = 0; i < par1.length; i++) {
 			if (i < par1.length - 1) {
 				if (par1[i] != null) {
-					var1 += par1[i] + ":";
+					var1 += par1[i] + SEPARATOR;
 				} else {
-					var1 += "null" + ":";
+					var1 += "null" + SEPARATOR;
 				}
 			} else {
 				if (par1[i] != null) {
 					var1 += par1[i];
+				} else {
+					var1 += "null";
+				}
+			}
+		}
+		return var1;
+	}
+	
+	public static final String toString(Enchantment... par1) {
+		String var1 = "";
+		for (int i = 0; i < par1.length; i++) {
+			if (i < par1.length - 1) {
+				if (par1[i] != null) {
+					var1 += toSimpleString(par1[i]) + SEPARATOR;
+				} else {
+					var1 += "null" + SEPARATOR;
+				}
+			} else {
+				if (par1[i] != null) {
+					var1 += toSimpleString(par1[i]);
 				} else {
 					var1 += "null";
 				}
@@ -54,9 +132,9 @@ public class DataFormatter {
 		for (int i = 0; i < par1.length; i++) {
 			if (i < par1.length - 1) {
 				if (par1[i] != null) {
-					var1 += toSimpleString(par1[i]) + ":";
+					var1 += toSimpleString(par1[i]) + SEPARATOR;
 				} else {
-					var1 += "null" + ":";
+					var1 += "null" + SEPARATOR;
 				}
 			} else {
 				if (par1[i] != null) {
@@ -67,6 +145,11 @@ public class DataFormatter {
 			}
 		}
 		return var1;
+	}
+	
+	@SuppressWarnings("deprecation")
+	public static final String toSimpleString(Enchantment par1) {
+		return "(" + toString(par1.getId(), par1.getItemTarget().toString()) + ")";
 	}
 	
 	public static final String toSimpleString(Location par1) {
@@ -82,7 +165,7 @@ public class DataFormatter {
 	}
 	
 	public static final String toString(Player par1) {
-		return toFormat("Player", "Name:" + par1.getName(), "Location:" + toSimpleString(par1.getLocation()), "Inventory:" + toSimpleString(par1.getInventory()));
+		return toFormat("Player", "Name" + VAR_SEP + par1.getName(), "Location" + VAR_SEP + toSimpleString(par1.getLocation()), "Inventory:" + VAR_SEP + toSimpleString(par1.getInventory()));
 	}
 	
 }
