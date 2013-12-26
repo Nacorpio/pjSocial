0bb3966ebd279ba3161120df7963ab7e73b512f1
Nacorpio <nacorpio@live.com>
2013-12-26 19:54:04 +0100

    Cleaned up and added comments
    I cleaned up the code a bit, and added some comments. I also retried
    using the format that I made, and it was working the way I was
    expecting.

--- src/com/nacorpio/pj/social/format/DataFormatter.java
+++ src/com/nacorpio/pj/social/format/DataFormatter.java
@@ -1,7 +1,6 @@
 package com.nacorpio.pj.social.format;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -13,7 +12,14 @@ import org.bukkit.inventory.ItemStack;
 
 public class DataFormatter {
 
+	/**
+	 * The separator used for splitting a value.
+	 */
 	public static final String VAR_SEP = "->";
+	
+	/**
+	 * The separator used for splitting the values.
+	 */
 	public static final String SEPARATOR = "&";
 	
 	static class Value {
@@ -26,10 +32,18 @@ public class DataFormatter {
 			this.value = par2;
 		}
 		
+		/**
+		 * Returns the name of the value.
+		 * @return the name.
+		 */
 		public final String getName() {
 			return this.name;
 		}
 		
+		/**
+		 * Returns the value of the value.
+		 * @return the value.
+		 */
 		public final Object getValue() {
 			return this.value;
 		}
@@ -55,14 +69,40 @@ public class DataFormatter {
 			}
 		}
 
+		/**
+		 * Returns the name of the FormatValue.
+		 * @return the name.
+		 */
 		public final String getName() {
 			return name;
 		}
 		
+		/**
+		 * Returns all the data values of the FormatValue.
+		 * @return the data values.
+		 */
 		public final List<Value> getDataValues() {
 			return data_values;
 		}
 		
+		/**
+		 * Returns the value with the specified parent name.
+		 * @param par1 the name of the parent.
+		 * @return the value.
+		 */
+		public final String getValue(String par1) {
+			for (String var1: values) {
+				if (var1.startsWith(par1 + "->")) {
+					return var1.split(Pattern.quote(VAR_SEP))[1];
+				}
+			}
+			return null;
+		}
+		
+		/**
+		 * Returns all values in unparsed form.
+		 * @return the values.
+		 */
 		public final String[] getValues() {
 			return values;
 		}
@@ -98,10 +138,21 @@ public class DataFormatter {
 		
 	}
 	
+	/**
+	 * Returns whether the specified String is of valid format.
+	 * @param par1 the String to check.
+	 * @return the end-result.
+	 */
 	public static final boolean isValidFormat(String par1) {
 		return (par1.startsWith("[") && par1.endsWith("}]") && par1.contains(":{"));
 	}
 	
+	/**
+	 * Serializes the String into readable format.
+	 * @param par1 the name of the FormatValue.
+	 * @param par2 the values of the FormatValue.
+	 * @return the unparsed String.
+	 */
 	public static final String toFormat(String par1, String... par2) {
 		String var1 = "";
 		if (!isEmpty(par1) && !isEmpty(par2)) {
@@ -110,12 +161,24 @@ public class DataFormatter {
 		return var1;
 	}
 
+	/**
+	 * Returns whether the specified format-string is of the specified type.
+	 * @param par1 the String to check.
+	 * @param par2 the type to check for.
+	 * @return the end-result.
+	 */
 	public static final boolean instanceOf(String par1, String par2) {
 		return par1.contains("[" + par2 + ":{") && par1.endsWith("}]");
 	}
 
 	// //
 
+	/**
+	 * Separates the specified objects into a String.<br>
+	 * Used for the format that is used for serialization.
+	 * @param par1 the array of objects to separate.
+	 * @return the end-result.
+	 */
 	public static final String toString(Object... par1) {
 		String var1 = "";
 		for (int i = 0; i < par1.length; i++) {
@@ -181,14 +244,29 @@ public class DataFormatter {
 		return "(" + toString(par1.getId(), par1.getItemTarget().toString()) + ")";
 	}
 
+	/**
+	 * Serializes the specified Location into a readable String.
+	 * @param par1 the Location to use.
+	 * @return the end-result.
+	 */
 	public static final String toSimpleString(Location par1) {
 		return "(" + toString(par1.getBlockX(), par1.getBlockY(), par1.getBlockZ(), par1.getYaw(), par1.getPitch()) + ")";
 	}
 
+	/**
+	 * Serializes the specified ItemStack into a readable String.
+	 * @param par1 the ItemStack to use.
+	 * @return the end-result.
+	 */
 	public static final String toSimpleString(ItemStack par1) {
 		return "(" + toString(par1.getType(), par1.getAmount(), par1.getItemMeta().getDisplayName(), par1.getItemMeta().getLore()) + ")";
 	}
 
+	/**
+	 * Serializes the specified Inventory into a readable String.
+	 * @param par1 the Inventory to use.
+	 * @return the end-result.
+	 */
 	public static final String toSimpleString(Inventory par1) {
 		return "(" + toString(par1.getContents()) + ")";
 	}
--- src/com/nacorpio/pj/social/format/FormatTester.java
+++ src/com/nacorpio/pj/social/format/FormatTester.java
@@ -5,9 +5,9 @@ import com.nacorpio.pj.social.format.DataFormatter.FormatValue;
 public final class FormatTester {
 
 	public static void main(String[] args) {
-		String var1 = DataFormatter.toFormat("Player", "Name->Aazk&Health->100&Height->25");
+		String var1 = DataFormatter.toFormat("Player", "Name->Aazk", "Health->100", "Height->25");
 		FormatValue var2 = DataFormatter.toFormatValue(var1);
-		System.out.println(var2.getDataValues().get(2).getValue());
+		System.out.println(var2.getValue("Health"));
 	}
 
 }
