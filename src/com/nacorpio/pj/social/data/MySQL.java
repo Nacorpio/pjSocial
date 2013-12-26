2c72fcb86d1f22e1dd519420a224c47198b0fb10
LightifyLife <mike@lightifylife.com>
2013-12-26 15:28:33 +0100

    Added MySQL class.

---
+++ src/com/nacorpio/pj/social/data/MySQL.java
@@ -0,0 +1,45 @@
+package com.nacorpio.pj.social.data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.nacorpio.pj.social.ProjectSocial;
+
+public class MySQL {
+	private ProjectSocial pj;
+	private String ip;
+	private String databaseName;
+	private String username;
+	private String password;
+	
+	public Connection con;
+
+	public MySQL(ProjectSocial pj, String ip, String databaseName, String username, String password) {
+		this.pj = pj;
+		this.ip = ip;
+		this.databaseName = databaseName;
+		this.username = username;
+		this.password = password;
+	}
+	
+	public void OpenConnection(){
+		try {
+			con = DriverManager.getConnection("jdbc:mysql://" + ip + ":3306/" + databaseName, username, password);
+		} catch (Exception e) {
+			e.printStackTrace();
+			//We should stop the server because MySQL is needed in order to function.
+			this.pj.getPluginLoader().disablePlugin(pj);
+		}
+	}
+
+	public void CloseConnection() {
+		try {
+			if (con != null && con.isClosed()) {
+				con.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
