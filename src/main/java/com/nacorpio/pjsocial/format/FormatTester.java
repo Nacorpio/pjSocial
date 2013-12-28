711837dfa8a4c2c36118009d1faf86af640fe5c3
Nacorpio <nacorpio@live.com>
2013-12-28 23:19:36 +0100

    Fixed a bug
    The abstract classes cannot be instantianted, so I had to remove the
    testing area.

--- src/main/java/com/nacorpio/pjsocial/format/FormatTester.java
+++ src/main/java/com/nacorpio/pjsocial/format/FormatTester.java
@@ -5,11 +5,7 @@ import com.nacorpio.pjsocial.serialization.Section;
 public final class FormatTester {
 
 	public static void main(String[] args) {
-		Section var1 = new Section();
-		var1.addProperty("Name: [Hello,Fishy]");
-		var1.addProperty("Health: [Kishy,Nishy]");
-		var1.addProperty("Position: [0,0,0,0,0]");
-		System.out.println(var1.getSource() + "\n" + var1.getArray("Position")[4]);
+		
 	}
 
 }
