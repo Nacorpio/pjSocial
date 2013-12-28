1504b040063312567e7be694a4726764f910e899
Nacorpio <nacorpio@live.com>
2013-12-29 00:41:36 +0100

    Added JSONUtils
    Added some new utilities to parse JSON.

---
+++ src/main/java/com/nacorpio/pjsocial/util/JSONUtil.java
@@ -0,0 +1,33 @@
+package com.nacorpio.pjsocial.util;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public final class JSONUtil {
+
+	// {"valid":"false","message":"The api entered in the config doesn't seem to exist in our database. Please verify that you've registered on our website and followed the setup wizard."}
+	public static final String getValue(String par1, String par2) {
+		String var1 = par1.split(Pattern.quote("{"))[1].split(Pattern.quote("}"))[0];
+		String[] var2 = var1.split(Pattern.quote(",")); // "valid":"false"
+		for (String var: var2) {
+			if (var.startsWith("\"" + par2)) {
+				String value = var.split(Pattern.quote(":"))[1];
+				return value.replace("\"", "");
+			}
+		}
+		return null;
+	}
+	
+	public static final HashMap<String, String> getValues(String par1) {
+		HashMap<String, String> values = new HashMap<String, String>();
+		String var1 = par1.split(Pattern.quote("{"))[1].split(Pattern.quote("}"))[0];
+		String[] var2 = var1.split(Pattern.quote(","));
+		for (String var: var2) {
+			String val = var.replace("\"", "");
+			values.put(val.split(Pattern.quote(":"))[0], val.split(Pattern.quote(":"))[1]);
+		}
+		return values;
+	}
+	
+}
