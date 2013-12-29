072282302559236ec54e4463db444f2eeb30a0fd
LightifyLife <mike@lightifylife.com>
2013-12-29 19:23:29 +0100

    Used DebugUtil.

--- src/main/java/com/nacorpio/pjsocial/ProjectSocial.java
+++ src/main/java/com/nacorpio/pjsocial/ProjectSocial.java
@@ -8,6 +8,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import com.nacorpio.pjsocial.api.Administrative;
 import com.nacorpio.pjsocial.config.ConfigHandler;
 import com.nacorpio.pjsocial.listener.ChatListener;
+import com.nacorpio.pjsocial.util.DebugUtil;
 
 public final class ProjectSocial extends JavaPlugin {
 
@@ -31,6 +32,7 @@ public final class ProjectSocial extends JavaPlugin {
 		}
 		
 		//DEBUG PURPOSES
+		DebugUtil.debug(var1);
 		plugin.getLogger().log(Level.INFO, "API-KEY: " + ConfigHandler.api_key);
 	}
 	
