11eca5dabece412ddc8b21d1abe0816eb4628b19
LightifyLife <mike@lightifylife.com>
2013-12-29 01:17:58 +0100

    Removed debug
    Fixed. Issue was a , in the sentence.

--- src/main/java/com/nacorpio/pjsocial/api/Administrative.java
+++ src/main/java/com/nacorpio/pjsocial/api/Administrative.java
@@ -2,7 +2,6 @@ package com.nacorpio.pjsocial.api;
 
 import java.util.HashMap;
 
-import com.nacorpio.pjsocial.ProjectSocial;
 import com.nacorpio.pjsocial.util.HttpUtil;
 import com.nacorpio.pjsocial.util.JSONUtil;
 
@@ -15,7 +14,6 @@ public class Administrative {
 		HashMap<String, String> error;
 		try {
 			String var1 = HttpUtil.getSource("http://pjsocial.lightifylife.com/api/initialize?apikey=" + api_key + "&version=" + plugin_version + "&hwid=" + hardware_id + "&port=" + server_port);
-			ProjectSocial.plugin.getLogger().info("HTTP CALL WORKED!");
 			return JSONUtil.getValues(var1);
 		} catch (Exception e) {
 			error = new HashMap<String, String>();
