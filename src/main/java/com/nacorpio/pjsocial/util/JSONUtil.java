d28d57a6992502ac45eb7d4dce2b32c5ce20dd4e
Nacorpio <nacorpio@live.com>
2013-12-29 20:09:29 +0100

    Improved isJSON()

--- src/main/java/com/nacorpio/pjsocial/util/JSONUtil.java
+++ src/main/java/com/nacorpio/pjsocial/util/JSONUtil.java
@@ -24,7 +24,7 @@ public final class JSONUtil {
 	}
 	
 	public static final boolean isJSON(String par1) {
-		return par1.matches("\\{(\".*?\")+\\}");
+		return par1.matches("\\{(\".*?\":\".*?\")+\\}");
 	}
 	
 	// {"inventory":["",""]}
