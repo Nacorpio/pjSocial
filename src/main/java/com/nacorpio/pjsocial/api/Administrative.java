71bb2f8a961a4864a8c2c6b62d32c2a52277b6d2
LightifyLife <mike@lightifylife.com>
2013-12-29 19:35:21 +0100

    Debug.

--- src/main/java/com/nacorpio/pjsocial/api/Administrative.java
+++ src/main/java/com/nacorpio/pjsocial/api/Administrative.java
@@ -2,6 +2,7 @@ package com.nacorpio.pjsocial.api;
 
 import java.util.HashMap;
 
+import com.nacorpio.pjsocial.util.DebugUtil;
 import com.nacorpio.pjsocial.util.HttpUtil;
 import com.nacorpio.pjsocial.util.JSONUtil;
 
@@ -14,6 +15,7 @@ public class Administrative {
 		HashMap<String, String> error = new HashMap<String, String>();
 		try {
 			String var1 = HttpUtil.getSource("http://pjsocial.lightifylife.com/api/initialize?apikey=" + api_key + "&version=" + plugin_version + "&hwid=" + hardware_id + "&port=" + server_port);
+			DebugUtil.debug(var1);
 			return JSONUtil.getValues(var1);
 		} catch (Exception e) {
 			error.put("valid", "false");
